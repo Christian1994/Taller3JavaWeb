357b8ca3570c77bd08d1e08632d7eedab9aa1db0
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-03-27 23:35:00 -0500

    Cliente y Lógica Docente creados en un 50%

---
+++ src/java/vista/DocenteVista.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import logica.DocenteLogicaLocal;
+import modelo.Docente;
+import org.primefaces.component.commandbutton.CommandButton;
+import org.primefaces.component.inputtext.InputText;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author NOREÑA
+ */
+@Named(value = "docenteVista")
+@RequestScoped
+public class DocenteVista {
+
+    private InputText txtDocumento;
+    private InputText txtNombre;
+    private InputText txtApellido;
+    private InputText txtCorreo;
+    private InputText txtTelefono;
+    private InputText txtProfesion;
+    private InputText txtClave;
+    
+    private CommandButton btnRegistrar;
+    private CommandButton btnModificar;
+    private CommandButton btnEliminar;
+    private CommandButton btnLimpiar;
+    
+    private List<Docente> listaDocentes;
+    private Docente selectedDocente;
+    
+    @EJB
+    DocenteLogicaLocal docenteLogica;
+    
+    public InputText getTxtDocumento() {
+        return txtDocumento;
+    }
+
+    public void setTxtDocumento(InputText txtDocumento) {
+        this.txtDocumento = txtDocumento;
+    }
+
+    public InputText getTxtNombre() {
+        return txtNombre;
+    }
+
+    public void setTxtNombre(InputText txtNombre) {
+        this.txtNombre = txtNombre;
+    }
+
+    public InputText getTxtApellido() {
+        return txtApellido;
+    }
+
+    public void setTxtApellido(InputText txtApellido) {
+        this.txtApellido = txtApellido;
+    }
+
+    public InputText getTxtCorreo() {
+        return txtCorreo;
+    }
+
+    public void setTxtCorreo(InputText txtCorreo) {
+        this.txtCorreo = txtCorreo;
+    }
+
+    public InputText getTxtTelefono() {
+        return txtTelefono;
+    }
+
+    public void setTxtTelefono(InputText txtTelefono) {
+        this.txtTelefono = txtTelefono;
+    }
+
+    public InputText getTxtProfesion() {
+        return txtProfesion;
+    }
+
+    public void setTxtProfesion(InputText txtProfesion) {
+        this.txtProfesion = txtProfesion;
+    }
+
+    public InputText getTxtClave() {
+        return txtClave;
+    }
+
+    public void setTxtClave(InputText txtClave) {
+        this.txtClave = txtClave;
+    }
+
+    public CommandButton getBtnRegistrar() {
+        return btnRegistrar;
+    }
+
+    public void setBtnRegistrar(CommandButton btnRegistrar) {
+        this.btnRegistrar = btnRegistrar;
+    }
+
+    public CommandButton getBtnModificar() {
+        return btnModificar;
+    }
+
+    public void setBtnModificar(CommandButton btnModificar) {
+        this.btnModificar = btnModificar;
+    }
+
+    public CommandButton getBtnEliminar() {
+        return btnEliminar;
+    }
+
+    public void setBtnEliminar(CommandButton btnEliminar) {
+        this.btnEliminar = btnEliminar;
+    }
+
+    public CommandButton getBtnLimpiar() {
+        return btnLimpiar;
+    }
+
+    public void setBtnLimpiar(CommandButton btnLimpiar) {
+        this.btnLimpiar = btnLimpiar;
+    }
+
+    public List<Docente> getListaDocentes() {
+        if(listaDocentes == null) {
+            try {
+                listaDocentes = docenteLogica.consultarTodos();
+            } catch (Exception ex) {
+                Logger.getLogger(DocenteVista.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return listaDocentes;
+    }
+
+    public void setListaDocentes(List<Docente> listaDocentes) {
+        this.listaDocentes = listaDocentes;
+    }
+
+    public Docente getSelectedDocente() {
+        return selectedDocente;
+    }
+
+    public void setSelectedDocente(Docente selectedDocente) {
+        this.selectedDocente = selectedDocente;
+    }    
+    
+    // Mostrar por interfaz la docente seleccionada
+    public void onRowSelect(SelectEvent event) {
+        this.selectedDocente = (Docente) event.getObject();
+        
+        this.txtDocumento.setValue(selectedDocente.getDocumentodocente());
+        this.txtNombre.setValue(selectedDocente.getNombredocente());
+        this.txtApellido.setValue(selectedDocente.getApellidodocente());
+        this.txtCorreo.setValue(selectedDocente.getCorreodocente());
+        this.txtTelefono.setValue(selectedDocente.getTelefonodocente());
+        this.txtProfesion.setValue(selectedDocente.getProfesiondocente());
+        this.txtClave.setValue(selectedDocente.getClavedocente());
+        
+        // Se deshabilita el botón registrar para permitir que la docente se puede modificar o eliminar       
+        this.btnRegistrar.setDisabled(true);
+        this.btnModificar.setDisabled(false);
+        this.btnEliminar.setDisabled(false);
+        this.txtDocumento.setDisabled(true);
+    }
+    
+    // Limpia los campos y reinicia los valores
+    public void limpiar(){
+        this.txtDocumento.setValue("");
+        this.txtNombre.setValue("");
+        this.txtApellido.setValue("");
+        this.txtCorreo.setValue("");
+        this.txtTelefono.setValue("");
+        this.txtProfesion.setValue("");
+        this.txtClave.setValue("");
+        
+        this.txtDocumento.setDisabled(false);
+        this.btnRegistrar.setDisabled(false);
+        this.btnModificar.setDisabled(true);
+        this.btnEliminar.setDisabled(true);
+    }
+    
+    // Método registrar
+    public void action_registrar(){
+
+    }
+    
+    // Método modificar
+    public void action_modificar(){
+
+    }
+    
+    // Método eliminar
+    public void action_eliminar(){
+
+    }
+    
+    /**
+     * Creates a new instance of DocenteVista
+     */
+    public DocenteVista() {
+    }
+    
+}
